f66ba57c80281a91db7288e772340e90e37cd865
Menna Afify <mennaafi@gmail.com>
2024-07-30 17:11:41 +0300

    fourth commit

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import streamlit as st
-from keras.models import load_model
+from tensorflow.keras.models import load_model  # استخدام tf.keras
 from PIL import Image
 import numpy as np
 
 from util import classify, set_background
 
 set_background('templates/bg.png')  
-
-
-
 
 # set title
 st.title('Potato disease Classification')
@@ -21,14 +18,18 @@ file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
 
 # load classifier
 model_path = 'model1/model_v1.keras'
-model = load_model(model_path) 
+#model = load_model(model_path) 
+try:
+    model = load_model(model_path)
+except Exception as e:
+    st.error(f"Error loading model: {e}")
+
 
 # load class names  
-
 class_names = {
-    0: "Potato___Early_blight",
-    1: "Potato___Late_blight",
-    2: "Potato___healthy"
+    0: "Potato____Early_blight",
+    1: "Potato____Late_blight",
+    2: "Potato____healthy"
 }
 
 print(class_names) 
@@ -38,11 +39,9 @@ if file is not None:
     image = Image.open(file).convert('RGB')
     st.image(image, use_column_width=False, width=300)
 
-
     # classify image
     class_name = classify(image, model, class_names)
 
     # write classification
     st.write("## {}".format(class_name))
-
 
